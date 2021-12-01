618fcaa5ed6d2ff4c1178bc18b9dc9cda7946193
Tim Jackson <timjacksonm@gmail.com>
2021-11-30 21:32:58 -0600

    fixes for error handling

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,7 +1,6 @@
 const Item = require('../models/item');
 const Color = require('../models/color');
 const ItemInstance = require('../models/iteminstance');
-const item = require('../models/item');
 const Shape = require('../models/shape');
 
 // Site Home
@@ -17,12 +16,14 @@ exports.item_list_get = async function (req, res) {
       Color.find(),
     ];
     const [items, colors] = await Promise.allSettled(promises);
-    if (items && colors) {
+    if (items.value.length && colors.value.length) {
       res.render('inventory', {
         title: 'Current Inventory',
-        item_list: items,
-        color_list: colors,
+        item_list: items.value,
+        color_list: colors.value,
       });
+    } else {
+      throw new Error('database returned empty array.');
     }
   } catch (e) {
     res.render('error', {
@@ -39,9 +40,18 @@ exports.item_view_get = async function (req, res) {
       .populate('item')
       .populate({ path: 'item', populate: { path: 'shape' } })
       .populate({ path: 'item', populate: { path: 'color' } });
-    res.render('item', { item_data: itemData[0].item, instance: itemData[0] });
+    console.log(itemData);
+    if (itemData.length) {
+      res.render('item', {
+        item_data: itemData[0].item,
+        instance: itemData[0],
+      });
+    }
   } catch (e) {
-    console.log('Error: ' + e.message);
+    res.render('error', {
+      message: 'Error displaying single item based on ID page',
+      error: e,
+    });
   }
 };
 
@@ -50,13 +60,15 @@ exports.create_item_get = async function (req, res) {
   try {
     const promises = [Color.find(), Shape.find()];
     const [colors, shapes] = await Promise.allSettled(promises);
-    if (colors && shapes) {
+    if (colors.value.length && shapes.value.length) {
       res.render('create_item_form', {
         title: 'Create A New Item',
         color_list: colors.value,
         shape_list: shapes.value,
         error: null,
       });
+    } else {
+      throw new Error('database returned empty array.');
     }
   } catch (e) {
     res.render('error', {
